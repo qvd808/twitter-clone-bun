5d3e6fbdede1084be82d194b6f90896f81acab56
Quang Vinh Dang <dqvinh808@gmail.com>
2023-09-28 12:29:09 -0700

    Update tailwind.config.js for intellisense

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
+  theme: {
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
       screens: {
         sm: '480px',
         md: '768px',
@@ -8,27 +16,14 @@ module.exports = {
         xl: '1440px',
       },
       colors: {
-        'blue': '#1fb6ff',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
+        'dark-grey': '#474d51',
+        'grey': '#212327',
+        'gray': '#808080',
       },
       fontFamily: {
         sans: ['Graphik', 'sans-serif'],
         serif: ['Merriweather', 'serif'],
       },
-      extend: {
-        spacing: {
-          '128': '32rem',
-          '144': '36rem',
-        },
-        borderRadius: {
-          '4xl': '2rem',
-        }
-      }
     }
   }
-  
+}
